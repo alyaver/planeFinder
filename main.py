0d340749d2ae8ecea09042d995047dac007a4a16
alyaver <64661819+alyaver@users.noreply.github.com>
2026-07-31 23:21:15 -0700

    the data now refreshes but the cards stack as 3 new cards are added without hte old ones being modified or destroyed

--- main.py
+++ main.py
@@ -4,43 +4,42 @@ import time
 import requests
 import datetime
 
-url = "https://api.airplanes.live/v2/point/37.619506/-122.374212/5"
-
+url = "https://api.airplanes.live/v2/point/38.75264976994842/-121.35877086617117/15"
 
 cleanedData = []
 
+def update_data():
+    try:
 
-try:
+        response = requests.get(url)
 
-    response = requests.get(url)
+        if response.status_code == 200:
 
-    if response.status_code == 200:
+            data = response.json()
+            sortedData = (sorted(data["ac"], key=lambda x: x["dst"]))
 
-        data = response.json()
-        sortedData = (sorted(data["ac"], key=lambda x: x["dst"]))
+            counter = 0
+            for x in sortedData:
+                tempData = {}
+                check = x.get("flight")
+                if not check:
+                    continue
+                tempData["flight"] = (x.get("flight")).strip()
+                tempData["lat"] = (x.get("lat"))
+                tempData["lon"] = (x.get("lon"))
+                tempData["dst"] = (x.get("dst"))
+                cleanedData.append(tempData)
+                counter = counter + 1
+                if counter == 3:
+                    break
 
-        counter = 0
-        for x in sortedData:
-            tempData = {}
-            check = x.get("flight")
-            if not check:
-                 continue
-            tempData["flight"] = (x.get("flight")).strip()
-            tempData["lat"] = (x.get("lat"))
-            tempData["lon"] = (x.get("lon"))
-            tempData["dst"] = (x.get("dst"))
-            cleanedData.append(tempData)
-            counter = counter + 1
-            if counter == 3:
-                break
+            print("Success")
+            print(cleanedData)
+        else:
+            print(f"Fail: {response.status_code}")
 
-        print("Success")
-        print(cleanedData)
-    else:
-        print(f"Fail: {response.status_code}")
-
-except requests.exceptions.RequestException as e:
-    print(f"A network error occurred: {e}")
+    except requests.exceptions.RequestException as e:
+        print(f"A network error occurred: {e}")
 
     
 #check to see if time has updated
@@ -49,6 +48,11 @@ def update_time():
         time_label.config(text=current_time)
         root.after(1000, update_time)  
 
+def refresh():
+    cleanedData.clear()
+    update_data()
+    make_new_cards()
+    root.after(15000, refresh)
 
 root = tk.Tk()
 w, h = root.winfo_screenwidth(), root.winfo_screenheight()
@@ -65,18 +69,19 @@ mainFrame = tk.Frame(root)
 mainFrame.configure(background='black')
 
 #create the flight cards, i = 3 so 3 cards will be created
-for i in cleanedData:
-     card = tk.Frame(mainFrame)
-     flightLabel = tk.Label(card, text=i["flight"], font=('Arial', 18), bg = '#000000', fg='#ffffff', background='#000000' )
-     flightLabel.pack()
-     latLabel = tk.Label(card, text=i["lat"], font=('Arial', 18), bg = '#000000', fg='#ffffff', background='#000000' )
-     latLabel.pack()
-     longLabel = tk.Label(card, text=i["lon"], font=('Arial', 18), bg = '#000000', fg='#ffffff', background='#000000' )
-     longLabel.pack()
-     distLabel = tk.Label(card, text=i["dst"], font=('Arial', 18), bg = '#000000', fg='#ffffff', background='#000000' )
-     distLabel.pack()
-     card.configure(background='black')
-     card.pack(pady=50)
+def make_new_cards():
+    for i in cleanedData:
+        card = tk.Frame(mainFrame)
+        flightLabel = tk.Label(card, text=i["flight"], font=('Arial', 18), bg = '#000000', fg='#ffffff', background='#000000' )
+        flightLabel.pack()
+        latLabel = tk.Label(card, text=i["lat"], font=('Arial', 18), bg = '#000000', fg='#ffffff', background='#000000' )
+        latLabel.pack()
+        longLabel = tk.Label(card, text=i["lon"], font=('Arial', 18), bg = '#000000', fg='#ffffff', background='#000000' )
+        longLabel.pack()
+        distLabel = tk.Label(card, text=i["dst"], font=('Arial', 18), bg = '#000000', fg='#ffffff', background='#000000' )
+        distLabel.pack()
+        card.configure(background='black')
+        card.pack(pady=50)
 
 #label for the time
 time_label = tk.Label(header, text="", font=('Arial', 24), bg = '#000000', fg='#ffffff', highlightbackground='#000000' )
@@ -88,6 +93,6 @@ title_label.pack(padx=0, pady=50, side="top")
 header.pack(fill='x')
 mainFrame.pack()
 
-
 update_time()
+refresh()
 root.mainloop()
